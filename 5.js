a8191e74fccb46bfeb2a82db46aac3d923121413
6Jonathan6 <jpr1993mm@hotmail.com>
2018-12-04 07:25:40 -0600

    Day 3

--- 5.js
+++ 5.js
@@ -1,33 +1,105 @@
-const claims = require('./dataDay3.js')
+const claims = require('./data1.js')
+// const example = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
+const example = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
+
+function range(start, final) {
+    const size = final - start
+    return new Array(size).fill(0).map((value, index) => {
+        return index + start
+    })
+}
 class Claim {
     constructor(string) {
         const array = string.split(' ')
-        this.id = Number(array[0].split('')[1])
-        this.x = Number(array[1].split('')[1])
-        this.y = Number(array[2].split('')[0])
+        this.id = Number(array[0].replace('#', ''))
+        this.x = Number(array[2].split(',')[0])
+        this.y = Number(array[2].split(',')[1].replace(':', ''))
         this.width = Number(array[3].split('x')[0])
         this.height = Number(array[3].split('x')[1])
     }
     get xLimit() {
-        return this.x + this.width
+        return this.x + this.width - 1
     }
     get yLimit() {
-        return this.y + this.height
+        return this.y + this.height - 1
     }
-    get area() {
-        return this.width * this.height
+    get xRange() {
+        const final = this.xLimit + 1
+        return range(this.x, final)
     }
-    overlap(claim) {
-        console.log(claim)
-        if ((claim.x >= this.x && claim.x <= this.xLimit) && (claim.y >= this.y && claim.y <= this.yLimit)) {
-            return true
-        }
-        return false
+    get yRange() {
+        const final = this.yLimit + 1
+        return range(this.y, final)
+    }
+    get points() {
+        const array = []
+        this.xRange.forEach(xcoord => {
+            this.yRange.forEach(ycoord => {
+                array.push([xcoord, ycoord])
+            })
+        })
+        return array
     }
 }
 
-const parsedClaims = claims.map((claim) => new Claim(claim))
-const example1 = new Claim('#1 @1, 3: 4x4')
-const example2 = new Claim('#2 @3, 1: 4x4')
-console.log(example1.yLimit)
-console.log(example1.overlap(example2))
+function parse(array) {
+    return array.map(value => new Claim(value))
+}
+
+function mapPoints(claims) {
+    const map = new Map()
+    const array = []
+    claims.forEach(claim => {
+        claim.points.forEach(point => {
+            array.push(point)
+            const stringPoint = JSON.stringify(point)
+            if (map.has(stringPoint)) {
+                let newValue = map.get(stringPoint).value + 1
+                map.set(stringPoint, { value: newValue, id: claim.id })
+            } else {
+                map.set(stringPoint, { value: 1, id: claim.id })
+            }
+        })
+    })
+    return map
+}
+
+function sumPoints(map) {
+    let count = 0
+    let noSharedPoints = new Map()
+    map.forEach((value, key) => {
+        if (value.value > 1) {
+            count += 1
+        } else {
+            if (noSharedPoints.has(value.id)) {
+                const array = noSharedPoints.get(value.id)
+                noSharedPoints.set(value.id, [...array, key])
+            } else {
+                noSharedPoints.set(value.id, [key])
+            }
+        }
+    })
+    return { count, noSharedPoints }
+}
+
+function composer(claims) {
+    const map = mapPoints(claims)
+    const { count, noSharedPoints } = sumPoints(map)
+    const arrayNosharedPoints = Array.from(noSharedPoints.keys())
+    let uniqueClaim
+    arrayNosharedPoints.forEach(id => {
+        const claim = claims.find(claim => claim.id === id)
+        const pointsLength = noSharedPoints.get(id).length
+        if (claim.points.length === pointsLength) {
+            uniqueClaim = claim
+        }
+    })
+    return { count: count, uniqueClaim }
+}
+
+
+const parsed = parse(claims)
+const { count, uniqueClaim } = composer(parsed)
+console.log('Shared', count)
+console.log('Unique', uniqueClaim)
+// console.log(getUniqueClaim(parsed))
